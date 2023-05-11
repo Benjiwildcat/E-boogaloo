d5735afc0287003d2ede0996f18f9d558cf97c44
Ben Richardson <benjiwildcat@gmail.com>
2023-05-10 21:29:05 -0500

    finished models

--- models/index.js
+++ models/index.js
@@ -5,13 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
 
+});
 // Categories have many Products
+Category.hasMany(Product, {
 
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Category, {
+  through: ProductTag,
+  onDelete: 'SET NULL'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  onDelete: 'SET NULL'
+});
 module.exports = {
   Product,
   Category,
